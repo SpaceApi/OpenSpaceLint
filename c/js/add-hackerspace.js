ade19db1776c90ae25b5672c739dc41e26d6735a
slopjong <slopjong>
2012-03-09 09:49:47 +0100

    'Add Space' feature is implemented :-)

--- c/js/add-hackerspace.js
+++ c/js/add-hackerspace.js
@@ -1,3 +1,8 @@
+/**
+ * This is the javascript part of the 'add new space' feature. It provides
+ * the #add=<url> route and adds a 'New Space' link to the DOM.
+ */
+
 $(document).ready(function(){	
 
     function addOnShowHandler(){
@@ -49,25 +54,58 @@ $(document).ready(function(){
     
     $.router(/^add=(.+)$/, function(m, url) {
 
+            // now enter the url in the text field and click
+            // on the validate button for the user
+            $("#json_input").val(url);
+            $("#validate").click();
+            
+            // bind a 'onShow' handler to the results to
+            // execute some code after the results are displayed
             addOnShowHandler();
-            // execute some code after the results are shown		    
             $("#results-specs-header-12").onShow(function(){
-                
+                       
                 // wait 1.5 seconds until the browser rendered the DIVs
+                // Update: 1.5s is not always enough.
+                // TODO: poll
                 setTimeout(function(){
                     var valid = $('div:contains("Your JSON is compliant")').size();
                     if(valid) {
-                        jQuery(".valid-overlay").data("overlay").load();
+                                            
+                        // set the space name and url in the form
+                        // to be sent to the add-hacker-space php script                 
+                        var json;
+                        try{
+                          var jsonVal = $('#json_input').val();
+                          json = JSON.parse(jsonVal);
+                        } catch(e){
+                          alert("Something went wrong: " + e.message);
+                          // no extra exception handling is required because
+                          // JSON should already be checked by json lint
+                          return;
+                        }
+                        if(json.hasOwnProperty("url")){
+                           // TODO: check if json.url is part of the passed url argument.
+                           //       This should prevent to override an existent entry
+                        }
+                        $("#add-hackerspace-url").val(url);
+                         
+                        if(json.hasOwnProperty("space"))
+                          $("#add-hackerspace-space").val(json.space);
+                        
+                      
+                        // reload the captcha and focus the text field
+                        Recaptcha.reload();
                         Recaptcha.focus_response_field();
+                        
+                        // show the overlay
+                        jQuery(".valid-overlay").data("overlay").load();
+                        
                     } else {
                         jQuery(".invalid-overlay").data("overlay").load();
                     }
                 }, 1500);
             
             }, true);
-            
-            $("#json_input").val(url);
-            $("#validate").click();
         
         }, function(m, url) {}
     );
@@ -81,12 +119,15 @@ $(document).ready(function(){
             callback: Recaptcha.focus_response_field
         });
     
+    // on submit
+    // add-space-form is the dialog with the captcha field
+    // not to confuse with the add space link with its input field
     $('#add-space-form').submit(function() {
         $.getJSON(
             "http://openspace.slopjong.de/add-hackerspace",
             $(this).serialize(),
             function(response){
-                //alert("success");
+
                 console.log(response);
                 
                 if(response.ok){
@@ -103,5 +144,63 @@ $(document).ready(function(){
         );
         
         return false;
-    });		
+    });
+    
+    $(".validateButtons .right").prepend('\
+            <span>\
+            <div id="add-space-input-box"><input type="url" id="add-space-input" type="text" value="Enter a URL providing a space api JSON."/><a href="#">Add</a><a href="#">Cancel</a></div>\
+            <a id="add-space-link" class="bold" href="#">New Space</a>\
+            </span>'
+    );
+
+    var input = $("#add-space-input");
+    
+    // initialize validator for a bunch of input fields
+    input.validator();
+    
+    input.click(function(){
+      if($(this).val() === "Enter a URL providing a space api JSON.")
+      {
+        $(this)
+          .val("")
+          .css("color","#333333");
+      }
+    });
+
+    input.blur(function(){
+      if($(this).val().length == 0){
+        $(this)
+          .val("Enter a URL providing a space api JSON.")
+          .css("color","#888888");
+      }
+    });
+    
+    $("#add-space-link").click(function(){
+      $("#add-space-input-box").show();
+      $(this).hide();
+    });
+
+    // the add button
+    $("#add-space-input-box a").first().click(function(){
+      
+      // perform validation programmatically
+      $("#add-space-input").data("validator").checkValidity();
+      var isUrl = $("#add-space-input").data("validator").checkValidity();
+      if(isUrl){
+        var url = $("#add-space-input").val();
+        if(url.indexOf("http") == -1)
+          url = "http://" + url;
+
+        window.location = "http://openspace.slopjong.de/#add=" + $("#add-space-input").val();
+        $("#add-space-input-box").hide();
+        $("#add-space-link").show();
+      }
+    })
+    
+    // the cancel button
+    $("#add-space-input-box a").last().click(function(){
+      $("#add-space-input-box").hide();
+      $("#add-space-link").show();      
+    });
+    
 });
--- c/php/add-hackerspace.php
+++ c/php/add-hackerspace.php
@@ -10,16 +10,40 @@ if(isset($_GET["recaptcha_response_field"])) {
         $resp = recaptcha_check_answer (
                 $privatekey,
                 $_SERVER["REMOTE_ADDR"],
-                $_GET["recaptcha_challenge_field"],
-                $_GET["recaptcha_response_field"]
+                stripslashes(strip_tags($_GET["recaptcha_challenge_field"])),
+                stripslashes(strip_tags($_GET["recaptcha_response_field"]))
         );
         
-        $response = array("ok" => false, "error" => "");
+        $response = array("ok" => false, "message" => "");
         
-        if ($resp->is_valid)
+        if ($resp->is_valid){
             $response["ok"] = true;
+            
+            // TODO: filter these variables
+            $url = stripslashes(strip_tags($_GET["url"]));
+            $space = stripslashes(strip_tags($_GET["space"]));
+
+            if(empty($url) || empty($space)){
+                $response["ok"] = false;
+                $response["message"] = "Incomplete data were sent to the server.";
+            }
+            else{
+                $file = "spacehandlers/directory.json";
+                $dirjson = file_get_contents($file);
+                $dirarr = json_decode($dirjson, true);
+                
+                if(!in_array($url, $dirarr)){
+                    $dirarr[$space] = $url;
+                    $dirjson = json_encode($dirarr);
+                    file_put_contents($file, $dirjson);
+                    $response["message"] = "The space got added to the directory0.";
+                }
+                else
+                    $response["message"] = "The space is already in the directory.";                            
+            }
+        }
         else
-            $response["error"] = $resp->error;
+            $response["message"] = $resp->error;
         
         //print_r($response);
         echo json_encode($response);        
