3ddc39ed53a3911549b62b2859a31f2dbbbee71a
slopjong <slopjong>
2012-04-15 02:00:00 +0200

    Now the directory has become selective. directory.json?space=syn2cat,RaumZeitLabor or directory.json?member=sensors.temperature or similar can be used.

--- c/php/spacehandlers/directory.php
+++ c/php/spacehandlers/directory.php
@@ -2,11 +2,54 @@
 
 error_reporting(0);
 
-// this script should synchronize with gmc's directory later
+// TODO: this script should synchronize with gmc's directory later
 
 header('Content-type: application/json');
 
-$directory = file_get_contents('directory.json');
-echo $directory;
+$directory_json = file_get_contents('directory.json');
+$directory_array = json_decode($directory_json, true);
+
+if(isset($_GET["space"]))
+{
+				$spaces = stripslashes(strip_tags($_GET["space"]));
+				$spaces = explode(",", $spaces);
+				
+				$arr = array();
+				foreach($spaces as $space)
+				{
+								$arr[$space] = $directory_array[$space];
+				}
+				echo json_encode($arr);
+				exit(0);
+}
+
+if(isset($_GET["member"]))
+{
+				$members = stripslashes(strip_tags($_GET["member"]));				
+				$members = explode(",", $members);
+				
+				$spaces = array();
+				foreach($members as $member)
+				{
+								$array_keys_json = file_get_contents("../cache/array_keys.json");
+								$array_keys_arr = json_decode($array_keys_json, true);
+								
+								if(count($spaces)==0)
+												$spaces = $array_keys_arr[1][$member];
+								else
+												$spaces = array_intersect($spaces, $array_keys_arr[1][$member]);
+				}
+				
+				$arr = array();
+				foreach($spaces as $space)
+				{
+								$arr[$space] = $directory_array[$space];
+				}
+				echo json_encode($arr);
+				exit(0);
+}
+
+// echo the full directory
+echo $directory_json;
 
 ?>
