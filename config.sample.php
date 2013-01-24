d9a04a8e6c4a816a406b584b4613f07ae05917b0
slopjong <slopjong@bands.lu>
2013-01-24 23:30:21 +0100

    Readme update & renamed the sample config file.

--- config.sample.php
+++ config.sample.php
@@ -6,13 +6,13 @@
 
 $recaptcha_key = array(
 				"public" => "6Lcsss8SAAAAAD4080SvtHcts5CBHyMhLU9q3eGS",
-				"private" => "6Lcsss8SAAAAAJ7Izqs_sQIgj91UyuY1AMsRisWy"				
+				"private" => "asdf87asd8ff0a89_asdf8ayusdufj13kj234kj"				
 				);
 
 /*****************************
  * Second-stage proxy
  *****************************/
-$apikey = "ce85b99cc46752fffee35cab9a7b0278abb4c2d2055cff685af4912c49490f8d";
+$apikey = "09a8fsdfy89a7sdf8usad9f76asd89f7as9d8fgs09fa7";
 $second_stage_proxy = "http://jasonproxy.herokuapp.com/?api=". $apikey ."&url=";
 
 ?>
