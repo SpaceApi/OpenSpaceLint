ff9454ce7a4b1347e8aec0d70e5439ea7432eac3
slopjong <git@slopjong.de>
2013-03-01 02:47:42 +0100

    Made the warnings of missing headers more clear

--- c/php/validation-plugins/headers-hosting-consideration.php
+++ c/php/validation-plugins/headers-hosting-consideration.php
@@ -19,22 +19,22 @@ function validate_headers_hosting_consideration($space_api_file, &$errors, &$war
         $headers = DataFetch::get_headers($url);
 
         if(!isset($headers["Access-Control-Allow-Origin"]))
-            $warnings[] = "The 'Access-Control-Allow-Origin' header is not set!";
+            $warnings[] = "The 'Access-Control-Allow-Origin' header (with a preferred value of '*') is not set!";
             
         if(isset($headers["Access-Control-Allow-Origin"]) && $headers["Access-Control-Allow-Origin"] != "*")
             $warnings[] = "The value of the 'Access-Control-Allow-Origin' header should be '*'.";
 
         if(!isset($headers["Content-Type"]))
-            $warnings[] = "The 'Content-Type' header is not set!";
+            $warnings[] = "The 'Content-Type' header (with a preferred value of 'application/json') is not set!";
             
         if(isset($headers["Content-Type"]) && strpos($headers["Content-Type"], "application/json") === false)
             $warnings[] = "The value of the 'Content-Type' header should be 'application/json'.";
             
         if(!isset($headers["Cache-Control"]))
-            $warnings[] = "The 'Cache-Control' header is not set!";
+            $warnings[] = "The 'Cache-Control' header (with a preferred value of 'no-cache') is not set!";
             
         if(isset($headers["Cache-Control"]) && $headers["Cache-Control"] != "no-cache")
-            $warnings[] = "The value of the 'Cache-Control' header should be 'no-cache'.";
+            $warnings[] = "The preferred value of the 'Cache-Control' header is 'no-cache'.";
     }
     else
         $logger->logNotice("The url for the space could not be determined.");
